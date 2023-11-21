3f861706c31aa07ce8509ce8d23ec8dd1d82ceb4
Fares Dries <faresdries@gmail.com>
2023-11-21 16:17:05 +0100

    Exo_Js_Part3

--- 07-Javascript/Exercice/Exo_9/main.js
+++ 07-Javascript/Exercice/Exo_9/main.js
@@ -1,4 +1,6 @@
 let userValue = Number(prompt("Veuillez entrez la valeur de N"))
+let longueur= Number(0);
+
 for(let i=1;i<=userValue;i++){
     console.log("Table des " + i + " : \n");
     for(let j=1;j<11;j++){
@@ -8,21 +10,41 @@ for(let i=1;i<=userValue;i++){
 }
 
 let result = '';
+for(let toto= 1;toto<52;toto++){
+    result +=('-');
+}
 for (let i = 0; i <= userValue; i++) {
 
+    
     for (let j = 0; j < 11; j++) {
         if(j==0 && i==0){
+            result+='\n'
             for(let toto= 1;toto<11;toto++){
-                result +=(toto+'\t');
-            }result+='\n';
+                const tmp=`${toto}`;
+            const tmp2 = tmp.padStart(4,' ');
+                result +=('|'+tmp2);
+                if(toto==10){
+                    result+='|'
+                    longueur=result.length-52
+                }
+            }
+            
             }
        
-        if(j == 0 && i>0){
         
-        } 
         else if(i>0 && j>0){
-        result += (i*j) + '\t';
+            const tmp=`${i*j}`;
+            const tmp2 = tmp.padStart(4,' ');
+        result += '|'+tmp2 ;
+        if(j==10){
+            result+='|'
         }
+        }
+        
+    }
+    result += '\n'
+    for(let toto= 1;toto<longueur+1;toto++){
+        result +=('-');
     }
     result += '\n'
 }
